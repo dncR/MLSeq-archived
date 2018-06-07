63703253e07f901048a30ff891282ec8b7e7aa76
Dincer GOKSULUK <dincer.goksuluk@gmail.com>
2018-06-07 10:42:06 +0300

    Adds package startup message. Minor changes in package vignette.

---
+++ R/onAttach.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("\n\nNOTE: MLSeq has major changes after version 1.20.1 and this will bump following versions to 2.y.z in the next release of Bioconductor (ver. 3.8). Most of the functions from previous versions were changed and new functions are included. Please see Beginner's Guide before continue with the analysis.")
+}
